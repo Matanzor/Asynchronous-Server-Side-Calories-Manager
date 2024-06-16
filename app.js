893d278dbf773fc77d1bcca7a66e6d7d18ee7444
Matanzor <121384944+Matanzor@users.noreply.github.com>
2024-06-16 20:37:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const apiRoutes = require('./routes/api');
+const apiRoutes = require('./routes/api');  // Ensure this path is correct
 
 const app = express();
 
@@ -10,22 +10,21 @@ app.use(bodyParser.json());
 
 // MongoDB connection
 const mongoUri = 'mongodb+srv://admin:admin123123@cluster0.xrhcmq8.mongodb.net/caloriesDB?retryWrites=true&w=majority&appName=Cluster0';
-mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('MongoDB is connected');
-}).catch(err => {
-    console.log('MongoDB connection error: ', err);
-});
-
-// Routes
-app.use('/api', apiRoutes);
+mongoose.connect(mongoUri)
+    .then(() => {
+        console.log('MongoDB is connected');
+    })
+    .catch((err) => {
+        console.log('MongoDB connection error: ', err);
+    });
 
 // Default route
 app.get('/', (req, res) => {
     res.send('Calories Manager RESTful Web Services');
 });
+
+// Use API routes without the /api prefix
+app.use('/', apiRoutes);
 
 // Start the server
 const port = process.env.PORT || 3000;
