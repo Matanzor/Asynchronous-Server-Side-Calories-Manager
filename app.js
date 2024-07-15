b7204a4260f547517cffff9b0fcd3f8c9e73b192
Matanzor <121384944+Matanzor@users.noreply.github.com>
2024-07-15 16:07:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+//matan itzhaki, 315529719
+//ofek ezra, 20849336
+//ido michael barnea, 316315837
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const apiRoutes = require('./routes/api');  // Ensure this path is correct
+const apiRoutes = require('./routes/api'); 
 
 const app = express();
 
@@ -9,7 +13,8 @@ const app = express();
 app.use(bodyParser.json());
 
 // MongoDB connection
-const mongoUri = 'mongodb+srv://admin:admin123123@cluster0.xrhcmq8.mongodb.net/caloriesDB?retryWrites=true&w=majority&appName=Cluster0';
+const mongoUri =
+ 'mongodb+srv://admin:admin123123@cluster0.xrhcmq8.mongodb.net/caloriesDB?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(mongoUri)
     .then(() => {
         console.log('MongoDB is connected');
