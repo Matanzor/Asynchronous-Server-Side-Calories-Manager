87560920ac79638ce0898b4453abed21ff05ef48
Matanzor <121384944+Matanzor@users.noreply.github.com>
2024-07-15 16:06:58 +0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,7 @@
+//matan itzhaki, 315529719
+//ofek ezra, 20849336
+//ido michael barnea, 316315837
+
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
@@ -18,7 +22,8 @@ router.post('/addcalories', async (req, res) => {
         if (!user_id || !year || !month || !day || !description || !category || !amount) {
             throw new ApiException('Missing required fields', 400);
         }
-        const calorie = new Calorie({ user_id, year, month, day, description, category, amount });
+        const calorie = new Calorie(
+            { user_id, year, month, day, description, category, amount });
         await calorie.save();
         res.status(201).send(calorie);
     } catch (err) {
@@ -53,8 +58,9 @@ router.get('/report', async (req, res) => {
         const categories = ['breakfast', 'lunch', 'dinner', 'other'];
         const report = {};
 
-        for (let category of categories) {
-            report[category] = await Calorie.find({ user_id, year, month, category }, 'day description amount');
+        for (const category of categories) {
+            report[category] = await Calorie.find({ user_id, year, month, category },
+                 'day description amount');
         }
 
         res.send(report);
@@ -69,11 +75,14 @@ router.get('/report', async (req, res) => {
 
 // Get developer information
 router.get('/about', (req, res) => {
-    console.log('About endpoint was hit');  // Add a log to ensure this route is hit
+    console.log('About endpoint was hit'); 
     const developers = [
-        { firstname: 'matan', lastname: 'itzhaki', id: 315529719, email: 'matanitzhaki3@gmail.com' },
-        { firstname: 'ofek', lastname: 'ezra', id: 208494336, email: 'ofek040897@gmail.com' },
-        { firstname: 'ido michael', lastname: 'barnea', id: 316315837, email: 'idorr1234@gmail.com' }
+        { firstname: 'matan', lastname: 'itzhaki', id: 315529719,
+             email: 'matanitzhaki3@gmail.com' },
+        { firstname: 'ofek', lastname: 'ezra', id: 208494336,
+             email: 'ofek040897@gmail.com' },
+        { firstname: 'ido michael', lastname: 'barnea', id: 316315837,
+             email: 'idorr1234@gmail.com' }
     ];
     res.json(developers);
 });
